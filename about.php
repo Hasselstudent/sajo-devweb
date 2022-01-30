e00a77eb46b4944d1b9d67ecbfa0ff0e77daeac9
Hasselstudent <98157821+Hasselstudent@users.noreply.github.com>
2022-01-30 11:20:12 +0100

    Create footer.php

--- about.php
+++ about.php
@@ -1,27 +1,5 @@
 <?php include("header.php"); ?>
 <div class="contentbox">
-    <h1>Denna sida är under konstruktion...</h1><br>
+    <br><h1>Denna sida är under konstruktion...</h1><br>
 </div>
-
-<hr>
-
-<div>
-    <footer>Copyright © 2022
-        <address>
-            SaJo WebDev <br>
-        </address>
-    </footer>
-</div>
-<script>
-    function toggleNavLinks() {
-        var x = document.getElementById("myDropdownLinks");
-        if (x.style.display === "block") {
-            x.style.display = "none";
-        } else {
-            x.style.display = "block";
-        }
-    }
-</script>
-</body>
-
-</html>
+<?php include("footer.php"); ?>
--- index.php
+++ index.php
@@ -55,38 +55,4 @@
         </div>
     </div>
 </div>
-<br>
-<hr><br>
-<!--Box med kontaktinfo-->
-<div class="visitinfobox">
-    <div class="visitinfo">
-        <b>Besök oss:</b><br><br>
-        Fantasivägen 13<br>
-        12 345, Fiktivryda<br><br>
-        developers@sajowebdev.com<br>
-        070 - 123 456 78<br>
-    </div>
-</div>
-<br>
-<hr>
-<div>
-    <footer>Copyright © 2022
-        <address>
-            SaJo WebDev <br>
-        </address>
-    </footer>
-</div>
-<!--JavaScript för att visa/dölja hamburgarmenyn-->
-<script>
-    function toggleNavLinks() {
-        var x = document.getElementById("myDropdownLinks");
-        if (x.style.display === "block") {
-            x.style.display = "none";
-        } else {
-            x.style.display = "block";
-        }
-    }
-</script>
-</body>
-
-</html>
+<?php include("footer.php"); ?>
